b6303ef05289929693c269be3d1ea7728bf3a22a
Emerson Trindade <trinddade29@gmail.com>
2022-09-13 20:06:18 -0300

    install ts-node tools

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,11 @@ const app = express();
 
 app.get('/ads', (req, resp) => {
   return resp.json([
-    {id: 1, name: "Anúncio 1"},
-    {id: 2, name: "Anúncio 2"},
-    {id: 3, name: "Anúncio 3"}
+    { id: 1, name: "Anúncio 1" },
+    { id: 2, name: "Anúncio 2" },
+    { id: 3, name: "Anúncio 3" },
+    { id: 4, name: "Anúncio 4" },
+    { id: 5, name: "Anúncio 5" },
   ])
 })
 
