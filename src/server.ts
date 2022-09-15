9f81db5904598d539d5ddeb0ae08f770e18b7927
Emerson Trindade <trinddade29@gmail.com>
2022-09-14 22:58:32 -0300

    create routers

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,28 @@ import express from 'express';
 
 const app = express();
 
-app.get('/ads', (req, resp) => {
+app.get('/games', (req, resp) => {
+  return resp.json([]);
+});
+
+app.post('/ads', (req, resp) => {
+  return resp.status(201).json([]);
+});
+
+app.get('/games/:id/ads', (req, resp) => {
+  //const gameId = request.params.id
   return resp.json([
     { id: 1, name: "Anúncio 1" },
     { id: 2, name: "Anúncio 2" },
     { id: 3, name: "Anúncio 3" },
     { id: 4, name: "Anúncio 4" },
     { id: 5, name: "Anúncio 5" },
-  ])
-})
+  ]);
+});
+
+app.get('/ads/:id/discord', (req, resp) => {
+  //const adId = request.params.id
+  return resp.json([]);
+});
 
 app.listen(3333);
